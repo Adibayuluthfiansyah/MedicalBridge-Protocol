751ad1c633eaef3d72028605a75a49e7359178a0
Adibayu Luthfiansyah <adibayuluthfiansyah@gmail.com>
2026-01-15 20:44:27 +0700

    features/create-controller-docter-user-patient

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PatientsModule } from './patients/patients.module';
+import { DoctorsModule } from './doctors/doctors.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PatientsModule, DoctorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
